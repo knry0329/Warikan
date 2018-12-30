1182cdf3b0a2da2a973bf7e02624037b0ec0dc1e
knry0329 <bxnxsxenathxy@gmail.com>
2018-12-30 19:40:12 +0900

    first commit warikan.

---
+++ src/main/java/com/knr/warikan/WarikanApplication.java
@@ -0,0 +1,12 @@
+package com.knr.warikan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarikanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarikanApplication.class, args);
+	}
+}
