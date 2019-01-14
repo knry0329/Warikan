2718ae07c3f988b2cbe7f2249fae3631e4c8ef78
knry0329 <bxnxsxenathxy@gmail.com>
2019-01-14 14:51:50 +0900

    add protect csrf fnc

--- src/main/java/com/knr/warikan/controller/SignUpController.java
+++ src/main/java/com/knr/warikan/controller/SignUpController.java
@@ -1,6 +1,7 @@
 package com.knr.warikan.controller;
 
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.knr.warikan.entity.MUserEntity;
+import com.knr.warikan.form.ErrorForm;
 import com.knr.warikan.form.FlgForm;
 import com.knr.warikan.form.LoginForm;
 import com.knr.warikan.repositories.MUserRepository;
 
 @Controller
-public class SignUpController {
+public class SignUpController extends BaseController {
 
 	@Autowired
 	MUserRepository repository;
@@ -27,12 +29,17 @@ public class SignUpController {
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	@Transactional(readOnly=false)
-	public String signup(@ModelAttribute("formModel") MUserEntity mUser) {
+	public String signup(@ModelAttribute("formModel") MUserEntity mUser, HttpServletRequest request, Model model) {
+		
+		if(!originHeaderCheck(request)) {
+			ErrorForm eForm = new ErrorForm();
+			eForm.setMsg("異なるドメインからのアクセスの可能性があります。");
+			model.addAttribute("errorForm",eForm);
+			return "error";
+		}
 		
 		repository.saveAndFlush(mUser);
-
 		session.setAttribute("user", mUser.getUser_id());
-
 		return "index";
 	}
 
