856a82c245ecec0857a787d2b659dfdf6a789668
金成祐介 <kanariyusuke@knry-mbp.local>
2019-07-15 05:51:48 +0900

    add insert-record sql file at docker-initialize .

---
+++ docker/db/03_insert_record.sql
@@ -0,0 +1,7 @@
+\c user1;
+
+insert into public.m_user values ('hoge', '$2a$10$fQWrLIHCMP0rfPpu8AcoPOGzOQYPGpWW47m.hWPvRdBLyTcyyzYS2');
+
+insert into public.t_expence values ('hoge',1,'田中','タクシー',10000);
+insert into public.t_expence values ('hoge',2,'鈴木','ホテル',20000);
+insert into public.t_expence values ('hoge',3,'鈴木','昼食',5000);
